8a251dfcee8a61c49aa085d99ca75edbb5d098b6
samuelwondimu <wsamuelwondimu@gmail.com>
2023-09-30 15:29:29 +0300

    feat: create product detail component

---
+++ src/components/product-card.tsx
@@ -0,0 +1,68 @@
+interface Product {
+  id: number;
+  name: string;
+  href: string;
+  price: string;
+  imageSrc: string;
+  imageAlt: string;
+}
+export default function ProductCard(props: Product) {
+  const { id, name, href, price, imageAlt, imageSrc } = props;
+  return (
+    <a key={id} href="/product" className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <img
+          src={imageSrc}
+          alt={imageAlt}
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{price}</p>
+    </a>
+  );
+}
+
+export const dataProduct = [
+  {
+    id: 1,
+    name: "Earthen Bottle",
+    href: "#",
+    price: "$48",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  },
+  {
+    id: 2,
+    name: "Nomad Tumbler",
+    href: "#",
+    price: "$35",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    imageAlt:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 3,
+    name: "Focus Paper Refill",
+    href: "#",
+    price: "$89",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+    imageAlt:
+      "Person using a pen to cross a task off a productivity paper card.",
+  },
+  {
+    id: 4,
+    name: "Machined Mechanical Pencil",
+    href: "#",
+    price: "$35",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt:
+      "Hand holding black machined steel mechanical pencil with brass tip and top.",
+  },
+  // More products...
+];
