8743d69eeeed5a0f774895e528a31b4141238264
samuelwondimu <wsamuelwondimu@gmail.com>
2023-10-02 13:50:14 +0300

    feat(page.tsx): fetch products from API using getProducts function and render them dynamically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
-import ProductCard, { dataProduct } from "@/components/product-card";
+import ProductCard from "@/components/product-card";
 import Image from "next/image";
+import { getProducts } from "@/services/products";
 
-export default function Home() {
+export default async function Home() {
+  const product = await getProducts();
   return (
     <main className="bg-white flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {dataProduct.map((product) => (
-            <ProductCard key={product.id} {...product} />
-          ))}
-          {dataProduct.map((product) => (
-            <ProductCard key={product.id} {...product} />
-          ))}
-          {dataProduct.map((product) => (
+          {product.map((product) => (
             <ProductCard key={product.id} {...product} />
           ))}
         </div>
