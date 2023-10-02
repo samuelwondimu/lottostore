f5530c4e93ca5343c1b1b4f5c7bba1bfd53273f7
samuelwondimu <wsamuelwondimu@gmail.com>
2023-10-02 13:51:50 +0300

    refactor(product-card.tsx): * - Import `Product` interface from `@/services/products` * - Import `Image` from "next/image" * - Import `Link` from "next/link" * - Change `imageSrc` to `image` in `props` * - Change `imageAlt` to `title` in `props` * - Replace `<a>` tag with `<Link>` component * - Add `width` and `height` props to `Image` component * - Change `name` to `title` in `<h3>` tag * - Remove unused `dataProduct` array

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,68 +1,22 @@
-interface Product {
-  id: number;
-  name: string;
-  href: string;
-  price: string;
-  imageSrc: string;
-  imageAlt: string;
-}
+import { Product } from "@/services/products";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function ProductCard(props: Product) {
-  const { id, name, href, price, imageAlt, imageSrc } = props;
+  const { id, image, price, title } = props;
   return (
-    <a key={id} href="/product" className="group">
+    <Link key={id} href={`/product/${id}`} className="group">
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-        <img
-          src={imageSrc}
-          alt={imageAlt}
+        <Image
+          width={500}
+          height={500}
+          src={image}
+          alt={title}
           className="h-full w-full object-cover object-center group-hover:opacity-75"
         />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+      <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">{price}</p>
-    </a>
+    </Link>
   );
 }
-
-export const dataProduct = [
-  {
-    id: 1,
-    name: "Earthen Bottle",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-  },
-  {
-    id: 2,
-    name: "Nomad Tumbler",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-  {
-    id: 3,
-    name: "Focus Paper Refill",
-    href: "#",
-    price: "$89",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 4,
-    name: "Machined Mechanical Pencil",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  // More products...
-];
