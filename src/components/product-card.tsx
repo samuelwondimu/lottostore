671d1dc2a1e86ad9f16e09bdc0d15d2013dab458
samuelwondimu <wsamuelwondimu@gmail.com>
2023-10-05 13:14:52 +0300

    refactor(product-card.tsx): update ProductCard component to use a new design for displaying product image and price

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -6,17 +6,19 @@ export default function ProductCard(props: Product) {
   const { id, image, price, title } = props;
   return (
     <Link key={id} href={`/product/${id}`} className="group">
-      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-        <Image
-          width={500}
-          height={500}
-          src={image}
-          alt={title}
-          className="h-full w-full object-cover object-center group-hover:opacity-75"
-        />
+      <div className="group relative overflow-hidden rounded-md bg-gray-800 transition-all">
+        <div className="relative block aspect-square landscape">
+          <Image
+            src={image}
+            alt={title}
+            className="object-cover transition-all"
+            fill
+            sizes="(max-width: 768px) 30vw, 33vw"
+          />
+        </div>
       </div>
       <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{price}</p>
+      <p className="mt-1 text-lg font-medium text-gray-900">{price} $</p>
     </Link>
   );
 }
