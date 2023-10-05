0f9db747cfe21753548638146512f45f1d9e3ffd
samuelwondimu <wsamuelwondimu@gmail.com>
2023-10-05 14:17:12 +0300

    refactor(cart-items.tsx): improve CartItem component structure and layout

--- src/components/cart-items.tsx
+++ src/components/cart-items.tsx
@@ -1,25 +1,36 @@
-"use client";
-import { Product } from "@/services/products";
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
+import { Product } from "@/services/products";
 
-function CartItem({ cartProduct }: { cartProduct: Product }) {
+interface CartItemProps {
+  cartProduct: Product & { quantity: number | undefined };
+}
+
+function CartItem({ cartProduct }: CartItemProps) {
   return (
     <Link
       href={`/product/${cartProduct?.id}`}
-      className="relative rounded-lg py-2 flex gap-2"
+      className="flex items-center p-4 border-b border-gray-600 transition duration-300"
     >
-      <Image
-        src={cartProduct?.image ?? ""}
-        alt={cartProduct?.title ?? ""}
-        width={50}
-        height={50}
-        className="rounded-lg"
-      />
-      <div>
-        <p className="font-bold">{cartProduct?.title}</p>
-        <p className="mt-1 text-gray-200">{cartProduct?.price}</p>
+      <div className="w-20 h-20 mr-4 relative">
+        <Image
+          src={cartProduct?.image ?? ""}
+          alt={cartProduct?.title ?? ""}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg"
+        />
+      </div>
+      <div className="flex flex-col flex-grow">
+        <p className="font-semibold">{cartProduct?.title}</p>
+        <p className="text-gray-200 text-sm mt-1">
+          Quantity: {cartProduct.quantity}
+        </p>
+        <p className="font-semibold mt-2">
+          {cartProduct.quantity} * ${cartProduct.price} = ${" "}
+          {(cartProduct.quantity! * cartProduct.price).toFixed(2)}
+        </p>
       </div>
     </Link>
   );
