e40727eadae68ac1f7b8f48d6675ecbb1ae6985e
samuelwondimu <wsamuelwondimu@gmail.com>
2023-10-05 15:26:16 +0300

    feat(cart-items.tsx): add remove from cart functionality

--- src/components/cart-items.tsx
+++ src/components/cart-items.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Product } from "@/services/products";
+import { useShoppingCart } from "@/context/cart-context";
 
 interface CartItemProps {
   cartProduct: Product & { quantity: number | undefined };
 }
 
 function CartItem({ cartProduct }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart();
   return (
     <Link
       href={`/product/${cartProduct?.id}`}
@@ -31,6 +33,27 @@ function CartItem({ cartProduct }: CartItemProps) {
           {cartProduct.quantity} * ${cartProduct.price} = ${" "}
           {(cartProduct.quantity! * cartProduct.price).toFixed(2)}
         </p>
+      </div>
+
+      <div className="flex flex-col items-end">
+        <button
+          className="text-gray-400 hover:text-gray-200"
+          onClick={() => removeFromCart(cartProduct.id)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 fill-current"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              // eslint-disable-next-line max-len
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm-4-8a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1z"
+            />
+          </svg>
+        </button>
       </div>
     </Link>
   );
