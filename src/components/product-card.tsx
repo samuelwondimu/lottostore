7c985e459d3a444a05d06a11dd6a368d50bef6ac
samuelwondimu <wsamuelwondimu@gmail.com>
2023-10-05 15:15:18 +0300

    chore(product-card.tsx): refactor ProductCard component to use aspect ratio and add focus styles

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -4,18 +4,17 @@ import Link from "next/link";
 
 export default function ProductCard(props: Product) {
   const { id, image, price, title } = props;
+
   return (
-    <Link key={id} href={`/product/${id}`} className="group">
-      <div className="group relative overflow-hidden rounded-md bg-gray-800 transition-all">
-        <div className="relative block aspect-square landscape">
-          <Image
-            src={image}
-            alt={title}
-            className="object-cover transition-all"
-            fill
-            sizes="(max-width: 768px) 30vw, 33vw"
-          />
-        </div>
+    <Link key={id} href={`/product/${id}`}>
+      <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+        <Image
+          src={image}
+          alt={title}
+          width={300}
+          height={300}
+          className="pointer-events-none object-cover group-hover:opacity-75"
+        />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">{price} $</p>
