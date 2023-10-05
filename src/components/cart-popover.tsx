49ff66908530d1a8c5fe104cc6d1947800890c8a
samuelwondimu <wsamuelwondimu@gmail.com>
2023-10-05 13:38:24 +0300

    refactor(cart-popover.tsx): improve styling and structure of CartPopover component * - Update Popover.Panel class to include flex-col and remove overflow-scroll * - Add overflow-scroll to the inner div element * - Add a new div element for the total and checkout button * - Style the total and checkout button div with background color, text color, padding, and rounded corners

--- src/components/cart-popover.tsx
+++ src/components/cart-popover.tsx
@@ -38,12 +38,17 @@ export default function CartPopover({
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
-        <Popover.Panel className="absolute flex mt-5 h-[50vh] overflow-scroll  rounded-3xl  bg-gray-800 w-screen max-w-max -translate-x-1/2 px-4">
-          <div className="w-screen max-w-sm flex-auto p-4 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-            {total}$
+        <Popover.Panel className="absolute flex flex-col mt-5 h-[50vh] rounded-3xl bg-gray-800 w-screen max-w-max -translate-x-1/2">
+          <div className="w-screen max-w-sm flex-auto p-4 text-sm leading-6 shadow-lg overflow-scroll ring-1 ring-gray-900/5 px-4">
             {data?.map((item) => (
               <CartItem key={item.id} cartProduct={item} />
             ))}
+          </div>
+          <div className="bg-gray-700 text-white p-4 mt-auto rounded-b-3xl flex justify-between">
+            <span>Total: ${total ? total.toFixed(2) : "0.00"}</span>
+            <button className="relative mt-4 inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+              Checkout
+            </button>
           </div>
         </Popover.Panel>
       </Transition>
