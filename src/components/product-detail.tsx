eda2b7d0ea69fefae68e16a05664410d7d84a7b0
samuelwondimu <wsamuelwondimu@gmail.com>
2023-10-05 13:15:17 +0300

    feat(product-detail.tsx): add functionality to increase cart quantity when Add to Cart button is clicked

--- src/components/product-detail.tsx
+++ src/components/product-detail.tsx
@@ -1,33 +1,36 @@
+"use client";
 import { Product } from "@/services/products";
 import Image from "next/image";
+import { useShoppingCart } from "@/context/cart-context";
 
 export default function ProductDetail(product: Product) {
+  const { increaseCartQuantity, cartItems } = useShoppingCart();
+  console.log(cartItems);
+
   return (
-    <div className="bg-white">
-      <div className="pt-6">
-        {/* Image gallery */}
-        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-4 lg:gap-x-8 lg:px-8">
-          <div className="col-span-2">
-            <Image
-              src={product.image}
-              alt={product.title}
-              width={500}
-              height={500}
-              className="rounded-lg"
-            />
-          </div>
-          <div className="col-span-2 gap-4">
-            <h3 className="text-gray-900 text-2xl font-extrabold pb-4">
-              {product.title}
-            </h3>
-            <p className="text-gray-900">{product.description}</p>
-            <button
-              type="button"
-              className="relative mt-4 inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-            >
-              Add to Cart
-            </button>
-          </div>
+    <div className="py-6 bg-white">
+      <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-4 lg:gap-x-8 lg:px-8">
+        <div className="col-span-2">
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={500}
+            height={500}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="col-span-2 gap-4">
+          <h3 className="text-gray-900 text-2xl font-extrabold pb-4">
+            {product.title}
+          </h3>
+          <p className="text-gray-900">{product.description}</p>
+          <button
+            onClick={() => increaseCartQuantity(product.id)}
+            type="button"
+            className="relative mt-4 inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
